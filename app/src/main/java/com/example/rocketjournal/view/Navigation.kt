7d45f2b8ed7f75e242eb1e601a25e5f1db7d509c
danielto2910 <dto1@ggc.edu>
2024-02-24 21:51:27 -0500

    navigation works between screens

--- app/src/main/java/com/example/rocketjournal/view/Navigation.kt
+++ app/src/main/java/com/example/rocketjournal/view/Navigation.kt
@@ -1,5 +1,8 @@
 package com.example.rocketjournal.view
 
+import AppBackgroundFront
+import AppBackgroundGeneral
+import LoginButtons
 import com.example.rocketjournal.MainActivity
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
@@ -20,26 +23,57 @@ fun Navigation(navController: NavHostController){
     NavHost(navController = navController, startDestination = "home") {
 
         composable("home") { MainDashboard(navController) }
+        composable("calendar") {  }
+        composable("list") {  }
+        composable("journal") {  }
+        composable("habit") {  }
+        composable("settings") {  }
+        composable("profile") {  }
+        composable("login") { LoginScreen(navController) }
+        composable("signup") {  }
         // Define other destinations here
+    }
+}
+@Composable
+fun LoginScreen(navController: NavController) {
+    AppBackgroundFront {
+        LoginButtons(navController)
+    }
+}
+
+@Composable
+fun SignUp(navController: NavController){
+    AppBackgroundGeneral {
+
     }
 }
 
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
-    Row {
-        Button(onClick = { }) {
+    Row (){
+        Button(onClick = {}) {
             Row {
-
-                Text(text = "Home")
+                Text(text = "Journals")
             }
         }
         Button(onClick = {}) {
             Row {
-
-                Text(text = "Settings")
+                Text(text = "List")
             }
         }
+        Button(onClick = { navController.navigate("home")}) {
+            Row {
+                Text(text = "Home")
+            }
+        }
+        Button(onClick = {navController.navigate("login")}) {
+            Row {
+                Text(text = "Login")
+            }
+        }
+
+
     }
 }
 
