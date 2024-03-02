e53815a0c62ec3083a8c4bed414af4e2d07492e9
bramoscazares <bramoscazares@gmail.com>
2024-03-02 11:59:01 -0500

    Editing JournalMainDash
    Making Dashboard more like the proto on figma, still missing the journal entries

--- app/src/main/java/com/example/rocketjournal/view/JournalMainDash.kt
+++ app/src/main/java/com/example/rocketjournal/view/JournalMainDash.kt
@@ -20,8 +20,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
@@ -31,11 +33,12 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
+
+
 @Preview
 @Composable
 fun JournalMainDash() {
     AppBackgroundGeneral {
-        var navController: NavController? = null
         val screenWidth = LocalConfiguration.current.screenWidthDp.dp
         val screenHeight = LocalConfiguration.current.screenHeightDp.dp
 
@@ -44,23 +47,52 @@ fun JournalMainDash() {
 
         Column {
 
+            //A back button
+            Button(
+                onClick = { /* Handle button click */ },
+                modifier = Modifier
+                    .offset(offsetX, screenHeight * 0.05f)
+                    .size(80.dp, 30.dp)
+                    .clipToBounds()
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(10.dp))
+                    .shadow(10.dp, RoundedCornerShape(10.dp)),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFB98231)
+                )
+            ) {
+                Text(
+                    text = "<",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    modifier = Modifier.run { align(Center) }
+
+                )
+            }
+
+            Spacer(modifier = Modifier.size(20.dp))
+
+            //A header that looks like a button that says Journals
             Button(
                 onClick = { /* Handle button click */ },
                 modifier = Modifier
                     .offset(offsetX, screenHeight * 0.05f)
                     .size(boxWidth, 60.dp)
                     .clipToBounds()
-                    .border(1.dp, Color.Black, shape = RoundedCornerShape(35.dp)),
-                shape = RoundedCornerShape(35.dp),
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(15.dp))
+                    .shadow(10.dp, RoundedCornerShape(15.dp)),
+                shape = RoundedCornerShape(15.dp),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color(0xFFB98231)
                 )
+                
             ) {
                 Text(
-                    text = "Journal",
+                    text = "Journals",
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    color = Color.Black
                 )
             }
 
@@ -79,36 +111,14 @@ fun JournalMainDash() {
                 )
             ) {
                 Text(
-                    text = "View Entries",
+                    text = "Entries",
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White
+                    color = Color.Black
                 )
             }
 
             Spacer(modifier = Modifier.size(20.dp))
-
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = Modifier
-                    .offset(offsetX, screenHeight * 0.05f)
-                    .size(boxWidth, 60.dp)
-                    .clipToBounds()
-                    .border(1.dp, Color.Black, shape = RoundedCornerShape(35.dp)),
-                shape = RoundedCornerShape(35.dp),
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFFB98231)
-                )
-            ) {
-                Text(
-                    text = "Settings",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White
-                )
-            }
         }
-
-        navController?.let { BottomNavigationBar(navController = it) }
     }
 }
