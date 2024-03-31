b51e4107b33550faa1a5caf7ffbc9369cd87ca1c
Juan Guevara <JGuevara7@ggc.edu>
2024-03-31 15:44:10 -0400

    Added functions to the date and time buttons (still not able to enter data to create the event) - Juan

--- app/src/main/java/com/example/rocketjournal/view/EventCreation.kt
+++ app/src/main/java/com/example/rocketjournal/view/EventCreation.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,17 +24,34 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 //import androidx.compose.material3.ExposedDropdownMenuDefaults.outlinedTextFieldColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberTimePickerState
 //import androidx.compose.material3.TextFieldDefaults.outlinedTextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterEnd
@@ -51,14 +70,19 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.rocketjournal.model.dataModel.EventData
 import com.example.rocketjournal.view.BottomNavigationBar
 import com.example.rocketjournal.view.HeaderRow
 import com.example.rocketjournal.viewmodel.EventViewModel
+import kotlinx.coroutines.launch
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EventCreation(navController: NavController) {
     AppBackgroundGeneral {
@@ -67,6 +91,37 @@ fun EventCreation(navController: NavController) {
 
         val boxWidth = screenWidth * 0.9f
         val offsetX = screenWidth * 0.05f
+
+        val sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = true
+        )
+        val scope = rememberCoroutineScope()
+
+            //Date and Time variables
+    val date = remember {
+        java.util.Calendar.getInstance().apply {
+            set(java.util.Calendar.YEAR, 2023)
+            set(java.util.Calendar.MONTH, 7)
+            set(java.util.Calendar.DAY_OF_MONTH, 23)
+        }.timeInMillis
+    }
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = date,
+        yearRange = 1990..2023
+    )
+    var showDatePicker by remember { mutableStateOf(false) }
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = 12,
+        initialMinute = 30,
+    )
+    var showTimePicker by remember { mutableStateOf(false) }
+
+    //mutable variables
+    var isDatePickerVisible by remember { mutableStateOf(false) }
+    var isTimePickerVisible by remember { mutableStateOf(false) }
+    val selectedDate by remember { mutableStateOf<LocalDate?>(null) }
+    var selectedTime by remember { mutableStateOf<LocalTime?>(null) }
 
 
         Column(modifier = Modifier
@@ -84,24 +139,19 @@ fun EventCreation(navController: NavController) {
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Spacer(modifier = Modifier.weight(1f))
-                // Title text aligned to the left
+                // Title text
                 TitleText(title = "Details")
 
-                // Spacer to push settings button to the right
-                //Spacer(modifier = Modifier.weight(1f))
-
-                // Settings button aligned to the right
+                // Cancel button aligned to the right
                 CancelButton(navController)
             }
             NameTextField()
             Spacer(modifier = Modifier.size(10.dp))
             DetailsTextField()
             Spacer(modifier = Modifier.size(20.dp))
-            DateButton()
-            Spacer(modifier = Modifier.size(10.dp))
-            TimeButton()
-            Spacer(modifier = Modifier.size(10.dp))
-            AddEntryButton()
+            Box(modifier = Modifier) {
+                DateTimePickerComponent()
+            }
         }
     }
 }
@@ -112,12 +162,12 @@ fun EventCreation(navController: NavController) {
 fun TitleText(title: String) {
     Box(
         modifier = Modifier
-            .padding(horizontal = 16.dp)
+            .padding(horizontal = 8.dp)
             .background(
                 color = Color.White.copy(alpha = 0.0f),
                 shape = RoundedCornerShape(15.dp),
             )
-            .padding(16.dp),
+            .padding(24.dp),
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -134,7 +184,7 @@ fun CancelButton(navController: NavController) {
     Button(
         onClick = { navController.navigate("event") },
         modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .padding(horizontal = 8.dp, vertical = 8.dp)
             .height(50.dp),
             //.align(Alignment.End),
         shape = RoundedCornerShape(60.dp),
@@ -194,57 +244,233 @@ fun DetailsTextField() {
     )
 }
 
+//@Composable
+//fun DateButton() {
+//    Button(
+//        onClick = { },
+//        shape = RoundedCornerShape(10.dp),
+//        colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
+//
+//        modifier = Modifier
+//            .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
+//            .padding(16.dp)
+//            .fillMaxWidth()
+//
+//    ) {
+//        Text(
+//            "Date",
+//            modifier = Modifier
+//                .fillMaxWidth(),
+//            fontWeight = FontWeight.Bold,
+//            style = TextStyle(
+//                color = Color.White,
+//                fontSize = 25.sp,
+//                textAlign = TextAlign.Center
+//            )
+//        )
+//    }
+//}
+
+//@Composable
+//fun TimeButton() {
+//    Button(
+//        onClick = { },
+//        shape = RoundedCornerShape(10.dp),
+//        colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
+//
+//        modifier = Modifier
+//            .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
+//            .padding(16.dp)
+//            .fillMaxWidth()
+//
+//    ) {
+//        Text(
+//            "Time",
+//            modifier = Modifier
+//                .fillMaxWidth(),
+//            fontWeight = FontWeight.Bold,
+//            style = TextStyle(
+//                color = Color.White,
+//                fontSize = 25.sp,
+//                textAlign = TextAlign.Center
+//            )
+//        )
+//    }
+//}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DateButton() {
-    Button(
-        onClick = { },
-        shape = RoundedCornerShape(10.dp),
-        colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
+fun DateTimePickerComponent() {
+    val datePickerState = rememberDatePickerState()
+    var showDatePicker by remember { mutableStateOf(false) }
+    var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
 
+
+    val timePickerState = rememberTimePickerState()
+    var showTimePicker by remember { mutableStateOf(false) }
+
+    Column(
         modifier = Modifier
-            .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
-            .padding(16.dp)
-            .fillMaxWidth()
-
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            "Date",
+
+
+        //Date Picker
+        Button(
+            onClick = {
+                showDatePicker = true //changing the visibility state
+            },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
+
             modifier = Modifier
-                .fillMaxWidth(),
-            fontWeight = FontWeight.Bold,
-            style = TextStyle(
-                color = Color.White,
-                fontSize = 25.sp,
-                textAlign = TextAlign.Center
+                .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
+                .padding(0.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Date",
+                style = TextStyle(
+                            color = Color.White,
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Center
+                ),
+                fontWeight = FontWeight.Bold
             )
-        )
+        }
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        //time picker
+        Button(
+            onClick = {
+                showTimePicker = true //changing the visibility state
+            },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
+
+            modifier = Modifier
+                .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
+                .padding(0.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Time",
+                style = TextStyle(
+                            color = Color.White,
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Center
+                ),
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        AddEntryButton()
+
     }
+
+    // date picker component
+    if (showDatePicker) {
+        DatePickerDialog(
+            onDismissRequest = { /*TODO*/ },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        showDatePicker = false
+                    }
+                ) { Text("OK") }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        showDatePicker = false
+                    }
+                ) { Text("Cancel") }
+            }
+        )
+        {
+            DatePicker(state = datePickerState)
+        }
+    }
+
+// time picker component
+    if (showTimePicker) {
+        TimePickerDialog(
+            onDismissRequest = { /*TODO*/ },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        showTimePicker = false
+                    }
+                ) { Text("OK") }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        showTimePicker = false
+                    }
+                ) { Text("Cancel") }
+            }
+        )
+        {
+            TimePicker(state = timePickerState)
+        }
+    }
+
+
+
 }
 
 @Composable
-fun TimeButton() {
-    Button(
-        onClick = { },
-        shape = RoundedCornerShape(10.dp),
-        colors = ButtonDefaults.buttonColors(Color.White.copy(alpha = 0.5f)),
-
-        modifier = Modifier
-            .background(Color.White.copy(alpha = 0.0f), shape = RoundedCornerShape(10.dp))
-            .padding(16.dp)
-            .fillMaxWidth()
-
+fun TimePickerDialog(
+    title: String = "Select Time",
+    onDismissRequest: () -> Unit,
+    confirmButton: @Composable (() -> Unit),
+    dismissButton: @Composable (() -> Unit)? = null,
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
     ) {
-        Text(
-            "Time",
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp,
             modifier = Modifier
-                .fillMaxWidth(),
-            fontWeight = FontWeight.Bold,
-            style = TextStyle(
-                color = Color.White,
-                fontSize = 25.sp,
-                textAlign = TextAlign.Center
-            )
-        )
+                .width(IntrinsicSize.Min)
+                .height(IntrinsicSize.Min)
+                .background(
+                    shape = MaterialTheme.shapes.extraLarge,
+                    color = AlertDialogDefaults.containerColor
+                ),
+            color = AlertDialogDefaults.containerColor
+        ) {
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 20.dp),
+                    text = title,
+                    style = MaterialTheme.typography.labelMedium
+                )
+                content()
+                Row(
+                    modifier = Modifier
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    dismissButton?.invoke()
+                    confirmButton()
+                }
+            }
+        }
     }
 }
 
@@ -274,3 +500,4 @@ fun AddEntryButton() {
         )
     }
 }
+
